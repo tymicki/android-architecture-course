ba10022444d1afb3b2ecd6b02e71b56379b9180d
jt <jt>
2020-04-04 11:20:44 +0200

    add base BaseObservableViewMvc and BaseViewMvc

--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListItemViewMvcImpl.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListItemViewMvcImpl.java
@@ -7,48 +7,26 @@ import android.widget.TextView;
 
 import com.techyourchance.mvc.R;
 import com.techyourchance.mvc.questions.Question;
+import com.techyourchance.mvc.screens.common.BaseObservableViewMvc;
 
-import java.util.ArrayList;
-import java.util.List;
+public class QuestionsListItemViewMvcImpl extends BaseObservableViewMvc<QuestionsListItemViewMvc.Listener>
+        implements QuestionsListItemViewMvc {
 
-public class QuestionsListItemViewMvcImpl implements QuestionsListItemViewMvc {
-
-    private final View mRootView;
-    private final List<Listener> mListeners = new ArrayList<>(1);
     private final TextView mTxtTitle;
     private Question mQuestion;
 
     public QuestionsListItemViewMvcImpl(LayoutInflater inflater, ViewGroup parent) {
-        mRootView = inflater.inflate(R.layout.layout_question_list_item, parent, false);
+        setRootView(inflater.inflate(R.layout.layout_question_list_item, parent, false));
         mTxtTitle = findViewById(R.id.txt_title);
         getRootView().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (Listener listener : mListeners) {
+                for (Listener listener : getListeners()) {
                     listener.onQuestionClicked(mQuestion);
                 }
 
             }
         });
-    }
-
-    private <T extends View> T findViewById(int id) {
-        return getRootView().findViewById(id);
-    }
-
-    @Override
-    public View getRootView() {
-        return mRootView;
-    }
-
-    @Override
-    public void registerListener(Listener listener) {
-        mListeners.add(listener);
-    }
-
-    @Override
-    public void unregisterListener(Listener listener) {
-        mListeners.remove(listener);
     }
 
     @Override
